ce353d86980275b3805fb7aab06f029658205ad1
Shivam <sg641818@gmail.com>
2025-06-29 00:11:24 +0530

    remove unwanted router

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import HomePage from './pages/HomePage';
 import BrowseCarsPage from './pages/BrowseCarsPage';
-import CarDetailsPage from './pages/CarDetailsPage';
-import BookingFlowPage from './pages/BookingFlowPage';
+
 import MyBookingsPage from './pages/MyBookingsPage';
 
 function App() {
@@ -13,8 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/browse" element={<BrowseCarsPage />} />
-        <Route path="/cars/:id" element={<CarDetailsPage />} />
-        <Route path="/booking" element={<BookingFlowPage />} />
         <Route path="/my-bookings" element={<MyBookingsPage />} />
       </Routes>
     </Router>
